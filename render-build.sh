39c421e1bce88fa225f3bffa99f28aa486e4d264
gus-m-wolf <gus.m.wolf@gmail.com>
2025-05-09 12:02:45 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 mkdir -p .render/chrome
 curl -SL https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o chrome.deb
 dpkg -x chrome.deb .render/chrome/
 rm chrome.deb
+
+mkdir -p .render/chrome/opt/google/chrome
+mv .render/chrome/opt/google/chrome/* .render/chrome/opt/google/
