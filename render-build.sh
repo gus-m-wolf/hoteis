291c7f737b937aa4e95d8465cf738ad8b9d5da19
gus-m-wolf <gus.m.wolf@gmail.com>
2025-05-09 12:05:59 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# Remove qualquer instalação anterior
+rm -rf .render/chrome
+
+# Cria a pasta e baixa o Chrome
 mkdir -p .render/chrome
 curl -SL https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o chrome.deb
 dpkg -x chrome.deb .render/chrome/
 rm chrome.deb
-
-mkdir -p .render/chrome/opt/google/chrome
-mv .render/chrome/opt/google/chrome/* .render/chrome/opt/google/
