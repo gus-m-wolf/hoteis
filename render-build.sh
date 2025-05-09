4100b69cf60078e6615a6824462672c5e7f029e1
gus-m-wolf <gus.m.wolf@gmail.com>
2025-05-09 11:46:01 -0300

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p .render/chrome
-curl -SL https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o chrome.deb
-dpkg -x chrome.deb .render/chrome/
-rm chrome.deb
