ba6d4eecc9d0827c1f89d92a4fb898f77cc5181f
gus-m-wolf <gus.m.wolf@gmail.com>
2025-05-09 12:58:41 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Remove qualquer instalação anterior
-rm -rf .render/chrome
+# Instalar dependências do Chrome
+apt-get update && apt-get install -y wget gnupg unzip curl
 
-# Cria a pasta e baixa o Chrome
-mkdir -p .render/chrome
-curl -SL https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o chrome.deb
-dpkg -x chrome.deb .render/chrome/
-rm chrome.deb
+# Baixar e instalar o Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+
+# Rodar o build padrão (ajuste conforme necessário)
+pip install -r requirements.txt
