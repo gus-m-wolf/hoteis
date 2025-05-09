8c9a16cb5525e54b65ea1b6a5082d496e27d70ec
gus-m-wolf <gus.m.wolf@gmail.com>
2025-05-09 12:59:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, jsonify
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.binary_location = "/usr/bin/google-chrome"
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
